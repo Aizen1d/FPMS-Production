4921bbcf98db29c4ed44fc99e994b3aaa439bdd5
Aizen1d <ddreataza@gmail.com>
2024-04-19 16:48:44 +0800

    New models added

---
+++ app/Models/AdminTasksResearchesPublished.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AdminTasksResearchesPublished extends Model
+{
+    use HasFactory;
+
+    protected $table = 'admin_tasks_researches_published';
+}
