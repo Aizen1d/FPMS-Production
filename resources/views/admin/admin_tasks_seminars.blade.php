75c184a4e2aa8dcf9d1c91edd8c5c90e6359a87c
David Daniel D. Reataza <108965326+Aizen1d@users.noreply.github.com>
2024-04-25 16:16:01 +0800

    Update admin_tasks_seminars.blade.php

--- resources/views/admin/admin_tasks_seminars.blade.php
+++ resources/views/admin/admin_tasks_seminars.blade.php
@@ -486,7 +486,7 @@
             loadingMessage();
 
             let token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-            fetch('/admin-tasks/create/seminars', {
+            fetch('/admin-tasks/seminars/create', {
                     headers: {
                         "Accept": "application/json, text-plain, */*",
                         "X-Requested-With": "XMLHttpRequest",
